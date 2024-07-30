c1bf6b48371e61e6b4d022dd6a4c580e753a6d06
Mustafa Can Gök <mustafacangok@hotmail.com>
2024-07-30 17:15:47 +0300

    Seçili olan sembol, tarih ve interval değerleri PK şeklinde düzenlendi.

---
+++ stockApp.sql
@@ -0,0 +1,7 @@
+-- SELECT * FROM public.mt5_db
+-- ORDER BY "time" ASC, symbol ASC, "interval" ASC 
+
+-- SELECT * FROM mt5_db WHERE interval = '15 minutes' 
+
+-- ALTER TABLE mt5_db
+-- ADD CONSTRAINT mt5_db_pkey PRIMARY KEY (time, symbol, interval);
