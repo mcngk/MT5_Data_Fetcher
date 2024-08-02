237da81ef051e3231c6687a5cac2f18f7f7e3291
Mustafa Can Gök <mustafacangok@hotmail.com>
2024-08-02 14:13:49 +0300

    Back-End Güncelleme İndikatörlerin grafiklerindeki kesişim noktaları belirlenerek kesişim noktalarının tarihleri listelenmesi yapıldı. crossover_dates_tb tablosu DB'ye eklendi.

--- stockApp.sql
+++ stockApp.sql
@@ -5,3 +5,23 @@
 
 -- ALTER TABLE mt5_db
 -- ADD CONSTRAINT mt5_db_pkey PRIMARY KEY (time, symbol, interval);
+
+-- CREATE TABLE crossover_dates_tb (
+--     symbol VARCHAR(10),
+--     date DATE
+-- );
+
+-- ALTER TABLE crossover_dates_tb
+-- ADD CONSTRAINT unique_symbol_date UNIQUE (symbol, date);
+
+-- ALTER TABLE crossover_dates_tb
+-- ALTER COLUMN date TYPE timestamp USING date::timestamp;
+
+-- SELECT * FROM public.crossover_dates_tb
+
+-- DELETE FROM crossover_dates_tb;
+
+-- ALTER TABLE crossover_dates_tb
+-- ADD CONSTRAINT crossover_dates_tb_pkey PRIMARY KEY (date, symbol);
+
+
